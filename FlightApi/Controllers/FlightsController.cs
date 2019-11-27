94b923e3978e8b0a853a069bce3edfbd41b60789
Ba Matrai <matrai.barnabas1@gmail.com>
2019-11-27 14:19:54 +0100

    add basic api request

---
+++ FlightApi/Controllers/FlightsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FlightApi.Models;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace FlightApi.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class FlightsController : ControllerBase
+    {
+        private readonly FlightContext _context;
+
+        public FlightsController(FlightContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Flights
+        [HttpGet]
+        public async Task<ActionResult<Flight>> GetFlights()
+        {
+            string path = "http://api.travelpayouts.com/v1/prices/cheap?origin=HKT&currency=RUB&page=None&token=3e08147c7f7449e03258a7b4daa9bdbf";
+            HttpClient client = new HttpClient();
+            string response = await client.GetStringAsync(path);
+            
+            JObject data = (JObject)JObject.Parse(response)["data"]["AER"]["1"];
+            string name = data.Property("airline").Value.ToString();
+
+            Flight flight = new Flight();
+
+            flight.Name = name.ToString();
+
+            _context.Flights.Add(flight);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetFlight), new { id = flight.Id }, flight);
+            //return await _context.Flights.ToListAsync();
+        }
+
+        // GET: api/Flights/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Flight>> GetFlight(long id)
+        {
+            var flight = await _context.Flights.FindAsync(id);
+
+            if (flight == null)
+            {
+                return NotFound();
+            }
+
+            return flight;
+        }
+
+        // PUT: api/Flights/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFlight(long id, Flight flight)
+        {
+            if (id != flight.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(flight).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FlightExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Flights
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Flight>> PostFlight(Flight flight)
+        {
+            _context.Flights.Add(flight);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetFlight), new { id = flight.Id }, flight);
+        }
+
+        // DELETE: api/Flights/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Flight>> DeleteFlight(long id)
+        {
+            var flight = await _context.Flights.FindAsync(id);
+            if (flight == null)
+            {
+                return NotFound();
+            }
+
+            _context.Flights.Remove(flight);
+            await _context.SaveChangesAsync();
+
+            return flight;
+        }
+
+        private bool FlightExists(long id)
+        {
+            return _context.Flights.Any(e => e.Id == id);
+        }
+    }
+}
