8658b7466498abdcc990e48d88c0677ac50ba740
Bálint Áts <atsbalint@gmail.com>
2019-12-09 11:18:46 +0100

    Checkout other branch, create UserModel class.

---
+++ FlightApi/UserModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlightApi
+{
+    public class UserContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source = user.db");
+    }
+
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public List<Ticket> Tickets { get; } = new List<Ticket>();
+    }
+
+    public class Ticket
+    {
+    }
+}
