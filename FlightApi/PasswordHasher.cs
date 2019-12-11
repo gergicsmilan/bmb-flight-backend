1142491bc8cdc26222a845b8c9e6c283b35041b7
Ba Matrai <matrai.barnabas1@gmail.com>
2019-12-11 15:57:29 +0100

    finish login feature

--- FlightApi/PasswordHasher.cs
+++ FlightApi/PasswordHasher.cs
@@ -9,19 +9,24 @@ namespace FlightApi
 {
     public class PasswordHasher
     {
+        private static string Salt = "$2a$10$rBV2JDeWW3.vKyeQcM8fFO";
+
         public static string CreateSaltedPasswordHash(string password)
         {
-            byte[] salt = new byte[16];
-            new RNGCryptoServiceProvider().GetBytes(salt);
+            string savedPasswordHash = BCrypt.Net.BCrypt.HashPassword(password, Salt);
 
-            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000);
-            byte[] hash = pbkdf2.GetBytes(20);
-
-            byte[] hashBytes = new byte[36];
-            Array.Copy(salt, 0, hashBytes, 0, 16);
-            Array.Copy(hash, 0, hashBytes, 16, 20);
-            string savedPasswordHash = Convert.ToBase64String(hashBytes);
             return savedPasswordHash;
+            //byte[] salt = new byte[16];
+            //new RNGCryptoServiceProvider().GetBytes(salt);
+
+            //var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000);
+            //byte[] hash = pbkdf2.GetBytes(20);
+
+            //byte[] hashBytes = new byte[36];
+            //Array.Copy(salt, 0, hashBytes, 0, 16);
+            //Array.Copy(hash, 0, hashBytes, 16, 20);
+            //string savedPasswordHash = Convert.ToBase64String(hashBytes);
+            //return savedPasswordHash;
         }
     }
 }
